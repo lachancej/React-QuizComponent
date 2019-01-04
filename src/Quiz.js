de48b898a63b6a27280bb4e8713a514831e0d24a
JENNIFER LACHANCE <JENNIFER.LACHANCE@LibertyMutual.com>
2019-01-04 13:07:09 -0500

    fix typo

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,7 @@ class Quiz extends Component {
     this.state = { quiz_position: 1 }
   }
 
-  const isQuizEnd = ((this.state.quiz_position - 1)) === quizData.quiz_questions.length)
+  const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
   render() {
     return (
       <div>
