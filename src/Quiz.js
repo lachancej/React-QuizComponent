3bcdb10b91e1d320421eb9e2cc0ee00ee4cf16a3
JENNIFER LACHANCE <JENNIFER.LACHANCE@LibertyMutual.com>
2019-01-07 09:46:46 -0500

    Move const declaration

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,8 +11,8 @@ class Quiz extends Component {
     this.state = { quiz_position: 1 }
   }
 
-  const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
   render() {
+    const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
     return (
       <div>
         {isQuizEnd ? <QuizEnd /> :
